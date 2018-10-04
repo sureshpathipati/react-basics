2661568b6c6421ea92b459d0a4836b5a79c6a181
pathipati suresh kumar <suresh@pathipatis-MacBook-Air.local>
2018-10-04 19:30:44 +0530

    Babel issue

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,11 @@
-console.log("Hurray! it works")
+import React,{ Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+	render() {
+		return (
+			<div> <h1>Suresh Kumar </h1></div>
+			);
+	}
+}
+render(<App/>, window.document.getElementById("app"));
